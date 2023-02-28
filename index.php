52de654bec75ee2016c6c55b1ecf788bc8b358af
TiparpronS <Sukanyat2022a@gmail.com>
2023-02-28 10:06:43 -0500

    first

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+class cupCake{
+   private $type = array("choclate", "rainbow", "mocha");
+   private $price = array(3, 4, 5);
+   private $stock = array(10,15,20);//check if we have cupcake in stock;
+private $tax= 7;
+private $discount = 5;// final price more than $20 get 5% off
+
+}
+
+?>
